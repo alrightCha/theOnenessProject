1b0c6549cacfa623ffa40d9e8456f08ca9fa59e4
alrightCha <ychamam7@gmail.com>
2022-12-17 17:28:03 +0100

    Adding some text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
         <h1>Welcome to the oneness project</h1>
+        <p>A new world order</p>
+        <p>Created by neonymph</p>
     </div>
   )
 }
